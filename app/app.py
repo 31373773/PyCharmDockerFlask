9f22239fa8578df4b73c9325f3c994614a0353ac
31373773 <55849726+31373773@users.noreply.github.com>
2020-04-21 20:42:34 -0400

    Tasks: update app.py, Dockerfile, init.sql, and docker-compose.yml

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from typing import List, Dict
 import mysql.connector
 import simplejson as json
 from flask import Flask, Response
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -29,13 +30,13 @@ def cities_import() -> List[Dict]:
 @app.route('/')
 def index():
     user = {'username': 'Miguel'}
-    cities_data = sities_import()
+    cities_data = cities_import()
 
     return render_template('index.html', title='Home', user=user, cities=cities_data)
 
 
 @app.route('/api/cities')
-def index() -> str:
+def cities() -> str:
     js = json.dumps(cities_import())
     resp = Response(js, status=200, mimetype='application/json')
     return resp
